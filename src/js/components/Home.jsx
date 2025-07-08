2d2d8129a1076ae0a94dee2b18218c6ac13bf2d5
Alejandro Lopez <alejo32267@gmail.com>
2025-07-07 23:52:18 -0400

    Done

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,49 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock } from '@fortawesome/free-solid-svg-icons';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+// Crea el componente SimpleCounter
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-            
+const SimpleCounter = (props) => {
+let clock = 6
+let counter = props.counter.toString().padStart(clock, "0").split("").map(Number)
+  return (
+    <div className="container">
+            <div className="card">
+                <h1><FontAwesomeIcon icon={faClock} flip="horizontal" /></h1>
+            </div>
+            <div className="card">
+                <h1>{counter[0]}</h1>
+            </div>
+            <div className="card">
+                <h1>{counter[1]}</h1>
+            </div>
+            <div className="card">
+                <h1>{counter[2]}</h1>
+            </div>
+            <div className="card">
+                <h1>{counter[3]}</h1>
+            </div>
+            <div className="card">
+                <h1>{counter[4]}</h1>
+            </div>
+            <div className="card">
+                <h1>{counter[5]}</h1>
+            </div>
+        </div>
+  );
+};
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+// Crea tu primer componente Home
+const Home = (props) => {
+  return (
+    <div>
+      <SimpleCounter 
+      {...props}
+      />
+    </div>
+	
+  );
 };
 
 export default Home;
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+
 //Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap"
 
 // index.css'
 import '../styles/index.css'
-
+import Home from './components/Home'
 // components
-import Home from './components/Home';
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
-)
+let counter = 0;
+setInterval(() => {
+  root.render(<Home counter={counter} />);
+  counter++;
+}, 1000);
